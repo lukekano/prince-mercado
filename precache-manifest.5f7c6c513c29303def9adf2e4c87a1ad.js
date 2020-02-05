a99973e4cb7d5c1ebb027df92eb6c817e3bf3c87
Prince Luke <lukekano@gmail.com>
2020-02-06 01:14:13 +0800

    Updates

---
+++ precache-manifest.5f7c6c513c29303def9adf2e4c87a1ad.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcc2f445d9275b2597320f8baac5b5b",
+    "url": "/prince-mercado/index.html"
+  },
+  {
+    "revision": "b55f0788cf6ff1f4ebcb",
+    "url": "/prince-mercado/static/css/2.d4ff16e3.chunk.css"
+  },
+  {
+    "revision": "363b83161c9e67fdd037",
+    "url": "/prince-mercado/static/css/main.2e08b87a.chunk.css"
+  },
+  {
+    "revision": "b55f0788cf6ff1f4ebcb",
+    "url": "/prince-mercado/static/js/2.b0802327.chunk.js"
+  },
+  {
+    "revision": "b4af2aee8742042dd787a9a9586740ea",
+    "url": "/prince-mercado/static/js/2.b0802327.chunk.js.LICENSE"
+  },
+  {
+    "revision": "363b83161c9e67fdd037",
+    "url": "/prince-mercado/static/js/main.e82fe21d.chunk.js"
+  },
+  {
+    "revision": "0be045535255bd485a1a",
+    "url": "/prince-mercado/static/js/runtime-main.fae98602.js"
+  },
+  {
+    "revision": "4fa977f99c2d2668a2e48e709f40ee5f",
+    "url": "/prince-mercado/static/media/hourglass.4fa977f9.png"
+  },
+  {
+    "revision": "8b427c54fdc56272626e8a3dc222dc12",
+    "url": "/prince-mercado/static/media/peoples.8b427c54.png"
+  },
+  {
+    "revision": "41517f0eb826ccf06f20cb94b0cfefba",
+    "url": "/prince-mercado/static/media/reset-password-icon.41517f0e.jpg"
+  },
+  {
+    "revision": "658c141c20143050e508bb6670ba3e8b",
+    "url": "/prince-mercado/static/media/sanpablo.658c141c.png"
+  },
+  {
+    "revision": "d7bf20e66b897e2e12ebf0803fd5cc84",
+    "url": "/prince-mercado/static/media/tricycle.d7bf20e6.png"
+  },
+  {
+    "revision": "a6513e6b68ff79675edf6d602b8f4d28",
+    "url": "/prince-mercado/static/media/trophy.a6513e6b.png"
+  }
+]);
