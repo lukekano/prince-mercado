6ab2362eb2da92e86da630d2e4dcc69e25167527
Prince Luke <lukekano@gmail.com>
2020-09-02 17:28:18 +0800

    added emailjs funtionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import emailjs from "emailjs-com";
 
 import Send from "@material-ui/icons/Send";
 
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+import ReCAPTCHA from "react-google-recaptcha";
+
 const useStyles = makeStyles((theme) => ({
   contactContainer: {
-    background: "#233",
+    background: "#212129",
     height: "100vh",
   },
   heading: {
-    color: "tomato",
+    color: "#3873B1",
     textAlign: "center",
     textTransform: "uppercase",
     marginBottom: "1rem",
@@ -30,8 +37,8 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     marginTop: "1rem",
-    color: "tomato",
-    borderColor: "tan",
+    color: "#3873B1",
+    borderColor: "#fff",
   },
   field: {
     margin: "1rem 0rem",
@@ -41,21 +48,21 @@ const useStyles = makeStyles((theme) => ({
 const InputField = withStyles({
   root: {
     "& label.Mui-focused": {
-      color: "tomato",
+      color: "#fff",
     },
     "& label": {
-      color: "tan",
+      color: "#3873B1",
     },
     "& .MuiOutlinedInput-root": {
       "& fieldset": {
-        borderColor: "tan",
+        borderColor: "#fff",
       },
       "&:hover fieldset": {
-        borderColor: "tan",
+        borderColor: "#fff",
       },
       "&.Mui-focused fieldset": {
         color: "#fff",
-        borderColor: "tan",
+        borderColor: "#fff",
       },
     },
   },
@@ -63,10 +70,66 @@ const InputField = withStyles({
 
 const Contact = () => {
   const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [captchaPassed, setCaptchaPassed] = useState(false);
+  const [open, setOpen] = useState(false);
+  const recaptchaRef = React.createRef();
+
+  const Alert = (props) => {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const recaptcha = recaptchaRef.current;
+    emailjs
+      .send(
+        "gmail",
+        "template_22kjqem",
+        {
+          name: name,
+          email: email,
+          message: message,
+        },
+        "user_qarjmh0NC3wdpJGnjQqM8"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setIsLoading(false);
+          setIsSuccess(true);
+          setOpen(true);
+          setName("");
+          setEmail("");
+          setMessage("");
+          recaptcha.reset();
+          setCaptchaPassed(false);
+        },
+        (error) => {
+          console.log(error.text);
+          setIsLoading(false);
+          setIsSuccess(false);
+          setOpen(true);
+        }
+      );
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
     <Box component="div" className={classes.contactContainer}>
       <Grid container justify="center">
-        <Box component="form" className={classes.form}>
+        <Box component="form" className={classes.form} onSubmit={onSubmitForm}>
           <Typography variant="h5" className={classes.heading}>
             Hire or Contact me...
           </Typography>
@@ -74,33 +137,68 @@ const Contact = () => {
             fullWidth={true}
             label="Name"
             variant="outlined"
+            name="name"
+            id="name"
+            required
+            autoComplete="off"
             inputProps={{ className: classes.input }}
+            value={name}
+            onInput={(e) => setName(e.target.value)}
           />
           <InputField
+            autoComplete="off"
             fullWidth={true}
             label="Email"
             variant="outlined"
+            id="email"
+            name="email"
+            required
+            type="email"
             inputProps={{ className: classes.input }}
             className={classes.field}
+            value={email}
+            onInput={(e) => setEmail(e.target.value)}
           />
           <InputField
+            autoComplete="off"
             fullWidth={true}
             label="Message"
             variant="outlined"
+            required
+            id="message"
+            name="message"
             multiline
             rows={4}
             inputProps={{ className: classes.input }}
+            value={message}
+            onInput={(e) => setMessage(e.target.value)}
           />
+          <ReCAPTCHA
+            ref={recaptchaRef}
+            style={{ marginTop: "1rem" }}
+            sitekey="6Ld5nMYZAAAAAIxN9dbgWayMpQQaYAqG4ChKSFtF"
+            onChange={() => setCaptchaPassed(true)}
+          />
+          ,
           <Button
             variant="outlined"
             fullWidth={true}
             endIcon={<Send />}
             className={classes.button}
+            type="submit"
+            disabled={!captchaPassed || isLoading}
           >
-            Contact Me
+            {isLoading ? <CircularProgress size="3rem" /> : "SEND"}
           </Button>
         </Box>
       </Grid>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={isSuccess ? "success" : "error"}>
+          {isSuccess
+            ? "Thanks for reaching out!, Ill get back to you soon."
+            : "Ooops! Something went wrong"}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 };
